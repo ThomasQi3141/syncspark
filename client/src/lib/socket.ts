67b31885b859f27bbf5ac79dc922621f9925b955
Thomas Qi <thomaszhaojieqi@gmail.com>
2025-05-13 19:31:11 -0400

    websockets troubleshooting

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -5,7 +5,7 @@ let socket: Socket;
 export function getSocket(): Socket {
   if (!socket) {
     // autoConnect: false so you can control when to open
-    socket = io(process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001", {
+    socket = io(process.env.NEXT_PUBLIC_WS_URL || "http://localhost:3001", {
       autoConnect: false,
       transports: ["websocket"],
     });
